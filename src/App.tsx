901d3d4a4665937806a2e6e6b4afd34f21161d3e
Nikolay Litvinov <nlit3926@gmail.com>
2024-08-14 20:38:44 +0200

    navigate

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
-import React, {useState} from 'react';
+import { Route, Routes, Navigate} from 'react-router-dom'
 import styles from "./components/Site.module.css";
+import {PageOne} from '../src/components/pages/PageOne'
+import {PageTwo} from '../src/components/pages/PageTwo'
+import {PageThree} from '../src/components/pages/PageThree'
+import {Error404} from '../src/components/pages/Error404'
+import React from "react";
+
 
 
 function App() {
@@ -8,16 +14,25 @@ function App() {
             <div className={styles.header}><h1>HEADER</h1></div>
             <div className={styles.body}>
                 <div className={styles.nav}>
-                    Здесь будет навигация
+                    <a href="/adidas">adidas</a>
+                    <a href="/puma">puma</a>
+                    <a href="/abibas">abibas</a>
                 </div>
                 <div className={styles.content}>
-                    Здесь будут кроссовки
-                </div>
+                    <Routes>
+                        <Route path='/' element={<Navigate to = '/adidas'/>}/>
+                        <Route path='/adidas' element={<PageOne/>}/>
+                        <Route path='/puma' element={<PageTwo/>}/>
+                        <Route path='/abibas' element={<PageThree/>}/>
+                        <Route path='/error404' element={<Error404/>}/>
+                        <Route path='/*' element={<Navigate to = {'/error404'}/>}/>
+                        </Routes>
+                    </div>
+                            </div>
+                            <div className={styles.footer}>abibas 2023</div>
             </div>
-            <div className={styles.footer}>abibas 2023</div>
-        </div>
-    );
-}
+            );
+            }
 
 
-export default App;
+            export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -8,8 +8,14 @@ import {BrowserRouter} from "react-router-dom";
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+
+
 root.render(
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+
   );
 
 // If you want to start measuring performance in your app, pass a function
