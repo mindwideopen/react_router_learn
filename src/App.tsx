8098e9e10387fc71875b7b3aab98b2ff4cb5fbf1
Nikolay Litvinov <nlit3926@gmail.com>
2024-08-15 16:46:05 +0200

    added NavLink

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { Route, Routes, Navigate} from 'react-router-dom'
+import {Route, Routes, Navigate, NavLink} from 'react-router-dom'
 import styles from "./components/Site.module.css";
-import {PageOne} from '../src/components/pages/PageOne'
-import {PageTwo} from '../src/components/pages/PageTwo'
-import {PageThree} from '../src/components/pages/PageThree'
-import {Error404} from '../src/components/pages/Error404'
+import {PageOne} from './components/pages/PageOne'
+import {PageTwo} from './components/pages/PageTwo'
+import {PageThree} from './components/pages/PageThree'
+import {Error404} from './components/pages/Error404'
 import React from "react";
 
 
@@ -14,22 +14,22 @@ function App() {
             <div className={styles.header}><h1>HEADER</h1></div>
             <div className={styles.body}>
                 <div className={styles.nav}>
-                    <a href="/adidas">adidas</a>
-                    <a href="/puma">puma</a>
-                    <a href="/abibas">abibas</a>
+                   <NavLink to={'/adidas'}>Adidas</NavLink>
+                   <NavLink to={'/puma'}>Puma</NavLink>
+                   <NavLink to={'/nike'}>Nike</NavLink>
                 </div>
                 <div className={styles.content}>
                     <Routes>
                         <Route path='/' element={<Navigate to = '/adidas'/>}/>
                         <Route path='/adidas' element={<PageOne/>}/>
                         <Route path='/puma' element={<PageTwo/>}/>
-                        <Route path='/abibas' element={<PageThree/>}/>
+                        <Route path='/nike' element={<PageThree/>}/>
                         <Route path='/error404' element={<Error404/>}/>
                         <Route path='/*' element={<Navigate to = {'/error404'}/>}/>
                         </Routes>
                     </div>
                             </div>
-                            <div className={styles.footer}>abibas 2023</div>
+                            <div className={styles.footer}>nike 2023</div>
             </div>
             );
             }
